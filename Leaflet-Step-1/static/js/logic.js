ff9778aa7952b9a481c73a7080e9463ab88dc562
Simon Feinsilver <sdfeinsilver@gmail.com>
2021-03-21 19:31:03 -0500

    Starting first part of homework

---
+++ Leaflet-Step-1/static/js/logic.js
@@ -0,0 +1,40 @@
+// Store query url that references earthquake info in variable
+const queryUrl = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson";
+
+// Perform a GET JSON request to the query url
+d3.json(queryUrl).then(data => {
+    // Once we get a response, send the data.features object to the createFeatures function that we will define later
+    createFeatures(data.features);
+});
+
+// Define the createFeatures function
+function createFeatures(earthquakeData) {
+
+    // Define a function we want to run once for each feature in the features array
+    // Give each feature a popup describing the place and time of the earthquake
+    function onEachFeature(feature, layer) {
+        layer.bindPopup("<h3>" + feature.properties.place +
+        "</h3><hr><p>" + new Date(feature.properties.time) + "</p>");
+    };
+
+    // Create a GeoJSON layer containing the features array on the earthquakeData object
+    // Run the onEachFeature function once for each piece of data in the array
+    let earthquakes = L.geoJSON(earthquakeData, {
+        onEachFeature: onEachFeature
+    });
+
+    // Sending our earthquakes layer to the createMap function
+    createMap(earthquakes);
+};
+
+function createMap(earthquakes) {
+    // Define streetmap and darkmap layers
+    let streetmap = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+    attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
+    tileSize: 512,
+    maxZoom: 18,
+    zoomOffset: -1,
+    id: "mapbox/streets-v11",
+    accessToken: API_KEY
+    })
+};
